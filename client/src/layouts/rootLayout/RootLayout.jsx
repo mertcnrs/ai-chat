b82437d27ac4dae14e403ede54510d3e92f11fe5
mertcnrs <mertcnr15@gmail.com>
2024-10-10 20:36:51 +0300

    TURK_Aİ

---
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -0,0 +1,38 @@
+import { Link, Outlet } from "react-router-dom";
+import "./rootLayout.css";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const queryClient = new QueryClient();
+
+const RootLayout = () => {
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <QueryClientProvider client={queryClient}>
+        <div className="rootLayout">
+          <header>
+            <Link to="/" className="logo">
+              <img src="https://assets-global.website-files.com/64b558e8890784c720b2f460/64b558e8890784c720b2f477_62e2b889a6aab1195ed93121_logoFlammini%20Design_logomark-white.svg" alt="" />
+              <span>TURK AI</span>
+            </Link>
+            <div className="user">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
